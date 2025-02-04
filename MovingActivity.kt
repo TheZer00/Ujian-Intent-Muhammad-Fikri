15d9e87f841aecb440dec7ca3e786a7d06cdbf39
TheZer00 <74489580+TheZer00@users.noreply.github.com>
2025-02-05 06:13:34 +0700

    MovingActivity.kt

---
+++ MovingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myintentappfikri
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_moving)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val kliktombol = findViewById<Button>(R.id.Tombolback)
+        kliktombol.setOnClickListener {
+            onklik()
+        }
+    }
+
+
+    private fun onklik() {
+        TODO("Not yet implemented")
+    }
+}
